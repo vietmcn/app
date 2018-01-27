23563bb0be64e722a2bb266b0e95471821303b9f
Vietmcn <vietmcn.com@gmail.com>
2018-01-27 23:54:33 +0700

    update

--- App/Control/class.header.php
+++ App/Control/class.header.php
@@ -5,13 +5,36 @@ if ( !class_exists('App_header') ) :
         public function __construct()
         {
             add_action( 'App_temp_header', array( $this, 'header_before' ) );
+            add_action( 'App_temp_header', array( $this, 'header_content' ) );
+            add_action( 'App_temp_header', array( $this, 'header_menu' ) );
         }
         public function header_before()
         {
-            $out  = '<div class="App-header-content-before App-socail">';
+            $out  = '<div class="App-header-content-before row no-gutters">';
+            $out .= '<div class="app-menu col-12 col-sm-6 col-md-8"></div>';
+            $out .= '<div class="App-socail col-6 col-md-4">';
             $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-facebook"></i></a>';
+            $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-twitter"></i></a>';
+            $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-googleplus"></i></a>';
+            $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-pinterest"></i></a>';
+            $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-instagram-outline"></i></a>';
+            $out .= '<a href="//fb.com/trangfox.xyz" title="Fanpage"><i class="ion-social-youtube"></i></a>';
+            $out .= '</div>';
             $out .= '</div>';
             echo $out;
+        }
+        public function header_content()
+        {
+            $out  = '<div class="App-header-contents container">';
+            $out .= '<div class="row">';
+            $out .= '<div data-elemt="logo" class="App-logo">'.get_the_title().'</div>';
+            $out .= '</div>';
+            $out .= '</div>';
+            echo $out;
+        }
+        public function header_menu()
+        {
+
         }
     }
     
