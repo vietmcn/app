7b5219112dfcd17eb45c8bb5fda1b818dae3b62b
Vietmcn <vietmcn.com@gmail.com>
2018-02-12 11:25:57 +0700

    update

--- App/Control/class.seo.php
+++ App/Control/class.seo.php
@@ -12,67 +12,28 @@ if ( !class_exists('App_control_seo') ) :
                 $this->app_seo_home();  
             } elseif ( is_category() ) {
                 $this->app_seo_category();
+            } elseif( is_tag() ) {
+                //
             } elseif ( is_single() ) {
                 $this->app_seo_single();
-            }
-            $this->app_seo_all();
-        }
-        private function app_seo_all()
-        {
-            $out = '<meta content="index,follow" name="robots">';
-            echo $out;
-        }
-        private function meta( $att = array() )
-        {
-            global $post, $App_getMetapost;
-            if ( $att['type'] == 'title' ) {
-                return $App_getMetapost->title( array(
-                    'post_id' => isset( $att['post_id'] ) ? $att['post_id'] : $post->ID,
-                    'key_name' => isset( $att['key_name'] ) ? $att['key_name'] : '_meta_seo', 
-                ) );
-            } elseif ( $att['type'] == 'desc' ) {
-                return $App_getMetapost->desc( array(
-                    'post_id' => $att['post_id'],
-                    'key_name' => $att['key_name'], 
-                ) );
-            } elseif ( $att['type'] == 'thumbnail' ) {
-                
             }
         }
         private function app_seo_home()
         {
             global $post, $App_setSeo;
             $site_name = explode( '//', get_bloginfo( 'name' ) );
-
-            $App_setSeo->title( array(
-                'title' => $this->meta( array(
-                    'post_id' => $post->ID,
+            $App_setSeo->meta( array( 
+                'title' => $App_setSeo->field( array(
                     'key_name' => '_meta_seo',
                     'type' => 'title',
                 ) ),
-            ) );
-            $App_setSeo->meta( array(
-                'desc' => $this->meta( array(
-                    'post_id' => $post->ID,
-                    'key_name' => '_meta_seo',
+                'desc' => $App_setSeo->field( array( 
                     'type' => 'desc',
-                ) ),
-                'title' => $this->meta( array(
-                    'type' => 'title',
-                ) ),
-                'site_name' => $site_name[0],
-                'url' => get_bloginfo( 'url' ),
-                'creator' => '@trangfox',
-                'card' => 'summary_large_image',
-                'type' => 'website',
-                'app_id' => '',
-                'admin_id' => '',
-                'alt' => $this->meta( array(
-                    'post_id' => $post->ID,
                     'key_name' => '_meta_seo',
-                    'type' => 'title',
-                 ) ),
-                'img' => 'https://lh3.googleusercontent.com/KHhaBnVpLpxQtm6Mo8W8dJH4vqqDaiahbZ_OnUCeZsKo_Jc4DfZ1Dez0ukT7VpKNtEBe=w300',
+                    'cat_id' => get_query_var( 'cat' ) ? get_query_var( 'cat' ) : '',
+                    'tag_id' => get_query_var( 'tag_id' ) ? get_query_var( 'tag_id' ) : '',
+                )),
+                'url' => get_bloginfo( 'url' ),
             ) );
         }
         private function app_seo_category()
@@ -81,43 +42,13 @@ if ( !class_exists('App_control_seo') ) :
             $cat_id = get_query_var( 'cat' );
             $cate_title = get_cat_name( $cat_id );
             $cat_custom = get_option( '_meta_cate_'.$cat_id );
-            var_dump( $cat_custom );
-            $App_setSeo->title( array(
-                'title' => !empty( $cate_custom['title'] ) ? $cat_custom['title'] : $cate_title,
-            ) );
-            $App_setSeo->meta( array( 
-                'title' => !empty( $cate_custom['title'] ) ? $cat_custom['title'] : $cate_title,
-                'desc' => '',
-            ) );
+            
         }
         private function app_seo_single()
         {
             global $post, $App_setSeo;
 
-            $title = $this->meta( array(
-                'post_id' => $post->ID,
-                'key_name' => '_meta_seo',
-                'type' => 'title',
-            ) );
-            $desc = $this->meta( array( 
-                'post_id' => $post->ID,
-                'key_name' => '_meta_seo',
-                'type' => 'desc',
-            ) );
-            $App_setSeo->title( 
-                array( 
-                    'title' => !empty( $title ) ? $title : get_the_title(),
-                )
-            );
-            $App_setSeo->meta( 
-                array( 
-                    'title' => !empty( $title ) ? $title : get_the_title(),
-                    'desc' => !empty( $desc ) ? $desc : get_the_excerpt(),
-                    'url' => get_permalink(),
-                    'type' => '',
-                    'img' => ''
-                )
-            );
+            
         }
     }
     
--- App/Mod/class.seo.php
+++ App/Mod/class.seo.php
@@ -2,14 +2,21 @@
 if ( !class_exists( 'App_seo' ) ) :
     class App_seo extends Models
     {
-        public function title( $atts = array() )
+        public function meta_all( $att = array() ) 
         {
-            $out = '<title>'.$atts['title'].'</title>';
+            $out  = '<meta property="og:site_name" content="'.$att['site_name'].'">';
+            $out .= '<meta content="index,follow" name="robots">';
+            $out .= '<meta property="fb:app_id" content="'.$att['app_id'].'">';
+            $out .= '<meta property="fb:admins" content="'.$att['admin_id'].'">';
+            $out .= '<meta property="og:locale" content="vi_VN">';
+            $out .= '<meta name="twitter:card" content="'.$att['card'].'" />';
+            $out .= '<meta name="twitter:creator" content="'.$att['creator'].'">';
             echo $out;
         }
         public function meta( $att = array() ) 
         {
-            $out  = '<link rel="canonical" href="'.$att['url'].'" >';
+            $out  = '<title>'.$att['title'].'</title>';
+            $out .= '<link rel="canonical" href="'.$att['url'].'" >';
             $out .= '<meta name="description" content="'.$att['desc'].'" />';
             $out .= '<meta itemprop="name" content="'.$att['site_name'].'" />';
             $out .= '<meta itemprop="description" content="'.$att['desc'].'" />';
@@ -19,17 +26,56 @@ if ( !class_exists( 'App_seo' ) ) :
             $out .= '<meta property="og:image" content="'.$att['img'].'">';
             $out .= '<meta property="og:image:alt" content="'.$att['alt'].'">';
             $out .= '<meta property="og:url" content="'.$att['url'].'">';
-            $out .= '<meta property="og:site_name" content="'.$att['site_name'].'">';
-            $out .= '<meta property="og:locale" content="vi_VN">';
-            $out .= '<meta property="fb:admins" content="'.$att['admin_id'].'">';
-            $out .= '<meta property="fb:app_id" content="'.$att['app_id'].'">';
             $out .= '<meta property="og:type" content="'.$att['type'].'">';
-            $out .= '<meta name="twitter:card" content="'.$att['card'].'" />';
             $out .= '<meta name="twitter:title" content="'.$att['title'].'" />';
             $out .= '<meta name="twitter:description" content="'.$att['desc'].'" />';
-            $out .= '<meta name="twitter:creator" content="'.$att['creator'].'">';
             $out .= '<meta name="twitter:image:src" content="'.$att['img'].'">';
             echo $out;
+        }
+        public function field( $att = array() ) 
+        {
+            global $post, $App_getMetapost;
+            if ( $att['type'] == 'title' ) {
+                $title = $App_getMetapost->title( array(
+                    'post_id' => isset( $att['post_id'] ) ? $att['post_id'] : $post->ID,
+                    'key_name' => isset( $att['key_name'] ) ? $att['key_name'] : '_meta_seo', 
+                ) );
+                if ( $title ) {
+                    return $title;
+                } else {
+                    if ( is_page() || is_front_page() ) {
+                        return get_the_title();
+                    } elseif( is_category() ) {
+                        return get_cat_name();
+                    } elseif( is_tag() ) {
+                        return single_tag_title( false );
+                    } elseif( is_single() ) {
+                        return get_the_title();
+                    } else {
+                        return 'Lổi rồi đại vương ơi!!';
+                    }
+                }
+            } elseif ( $att['type'] == 'desc' ) {
+                $desc = $App_getMetapost->desc( array(
+                    'post_id' => isset( $att['post_id'] ) ? $att['post_id'] : $post->ID,
+                    'key_name' => isset( $att['key_name'] ) ? $att['key_name'] : '_meta_seo', 
+                ) );
+                if ( $desc ) {
+                    return $desc;
+                } else {
+                    if ( is_page() || is_front_page() ) {
+                        return get_the_title();
+                    } elseif ( is_category() ) {
+                        return category_description( $att['cate_id'] );
+                    } elseif ( is_tag() ) {
+                        return tag_description( $att['tag_id'] );
+                    } elseif ( is_single() ) {
+                        return get_the_excerpt();
+                    }
+                }
+            } elseif ( $att['type'] == 'thumbnail' ) {
+                
+            }
         }
     }
     
